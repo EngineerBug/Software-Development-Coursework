f11a23f8d8ba8ecb577bbf75e3578c73f2bf93c9
EngineerBug <ben@finch.org.uk>
2022-10-29 16:02:55 +0100

    inputs for players and card file

--- src/Card.java
+++ src/Card.java
@@ -1,15 +1,32 @@
-public class Card{
+public class Card implements Comparable<Card>{
     private int value;
 
     public int getValue(){
         return this.value;
     }
 
-    public void Card(int value) {
+    public Card(int value){
         this.value = value;
     }
 
     public String toString(){
         return "The value of this card is: "+this.value;
+    }
+
+    @Override
+    /*
+     * The purpose of this method is to allow
+     * Card objects into queues.
+     * 
+     * When executing CardGame.main() without this method, 
+     * I get the error "class Card cannot be cast to class java.lang.Comparable"
+     * This error is thrown by the line "pack.add(nextCard);" in the generatePack method.
+     */
+    public int compareTo(Card card){
+        if (this.value == card.value){
+            return 1;
+        } else {
+            return 0;
+        }
     }
 }
--- src/CardGame.java
+++ src/CardGame.java
@@ -1,17 +1,78 @@
 import java.util.Scanner;
+import java.util.concurrent.PriorityBlockingQueue;
+import java.io.*;
 
 public class CardGame {
     public static void main(String[] args){
-        System.out.println("Hello Patryk, stop snooping on my code!");
+        System.out.println("Hello Patryk, what do you think if my code?");
         
-        //Take user input for player humber
-        Scanner myScanner = new Scanner(System.in);
-        System.out.println("Enter number of players: ");
-        String playerCount = myScanner.nextLine();
+        //create variables to store
+        Integer playerCount = 0;
+        String cardFile = "";
+        final PriorityBlockingQueue<Card> pack = new PriorityBlockingQueue<Card>();
+        Scanner scanner = new Scanner(System.in);
 
-        //Take user input for deck file
-        System.out.println("Enter input file for the cards: ");
-        String cardFile = myScanner.nextLine();
-        
+        //to prevent invalid inputs, a while loop checks.
+        Boolean value = false;
+        while (!value){
+            //Take user input for player number and pack file
+            playerCount = getPlayers(scanner);
+            cardFile = getPack(scanner);
+            generatePack(cardFile);
+
+            //check if the 
+            if (playerCount > 0 && pack.size() != 8*playerCount) {
+                System.out.println(playerCount.toString()+" players are playing with the pack "+cardFile);
+                value = true;
+            } else {
+                System.out.println("Please enter that again.");
+            }
+        }
+        //initialize the player threads
+
+        //deal to the players
+
+        //deal to the decks
+
+        //start() the player threads
+    }
+
+    static Integer getPlayers(Scanner scanPlayers){
+
+        //Take user input for player number
+        System.out.println("Enter number of players: ");
+        String x = scanPlayers.nextLine();
+        Integer output = Integer.valueOf(x);
+
+        return output;
+    }
+    static String getPack(Scanner scanPack){
+
+        //Take user input for pack file
+        System.out.println("Enter pack file: ");
+        String output = scanPack.nextLine(); //throws java.until.NoSuchElementException: No line found
+
+        return output;
+    }
+    static PriorityBlockingQueue<Card> generatePack(String fileName){
+        PriorityBlockingQueue<Card> pack = new PriorityBlockingQueue<Card>();
+        try{
+            //locate the file and store it
+            File file = new File(fileName);  
+            FileReader fileReader = new FileReader(file);
+            //create an object which can read the file     
+            BufferedReader br = new BufferedReader(fileReader);
+            String line;
+            while((line = br.readLine())!=null){
+                Card nextCard = new Card(Integer.valueOf(line));
+                pack.add(nextCard);
+            }  
+            fileReader.close();    //closes the stream and release the resources    
+        }  
+        catch(IOException e){  
+            pack.clear();
+        }
+
+        return pack;
     }
 }
