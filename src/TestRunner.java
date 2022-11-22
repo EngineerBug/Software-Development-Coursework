52a8be2d94000d28b8f58575274c74bfe277ea5f
Ben <ben@finch.org.uk>
2022-11-22 16:57:14 +0000

    cleaning up post-submission

--- src/TestRunner.java
+++ src/TestRunner.java
@@ -1,4 +1,3 @@
-/*
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
@@ -13,4 +12,4 @@ public class TestRunner {
 		
       System.out.println(result.wasSuccessful());
    }
-}*/
+}
